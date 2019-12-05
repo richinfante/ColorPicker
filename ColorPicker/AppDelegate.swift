cc77aeb298e166f7d0f8135a789251fe38bd7ed1
Rich Infante <rich@richinfante.com>
2019-12-05 16:47:58 -0500

    Update Swift version - add key shortcuts

--- ColorPicker/AppDelegate.swift
+++ ColorPicker/AppDelegate.swift
@@ -10,17 +10,50 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate {
-
-    func applicationDidFinishLaunching(aNotification: NSNotification) {
-        ColorPicker.sharedColorPanel().makeKeyAndOrderFront(self)
+    var lastMode : NSColorPanel.Mode = .wheel
+    
+    func applicationDidBecomeActive(_ notification: Notification) {
+        setupPanel()
+        NSColorPanel.shared.makeKeyAndOrderFront(self)
     }
 
-    func applicationWillTerminate(aNotification: NSNotification) {
-        ColorPicker.sharedColorPanel().close()
+    func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+        setupPanel()
+        NSColorPanel.shared.makeKeyAndOrderFront(self)
+        
+        return true
     }
     
-    @IBAction func openPalleteFolder(sender: AnyObject?){
-        let showFolder = NSTask()
+    func setupPanel() {
+        NSColorPanel.shared.delegate = self
+        NSColorPanel.shared.title = "Color Picker"
+        NSColorPanel.shared.showsAlpha = true
+        NSColorPanel.shared.hidesOnDeactivate = false
+        NSColorPanel.shared.isFloatingPanel = true
+        NSColorPanel.shared.titlebarAppearsTransparent = true
+        NSColorPanel.shared.isMovableByWindowBackground  = true
+        NSColorPanel.shared.standardWindowButton(NSWindow.ButtonType.miniaturizeButton)?.isEnabled = false
+        NSColorPanel.shared.standardWindowButton(NSWindow.ButtonType.closeButton)?.isEnabled = true
+    }
+    
+    
+    func applicationWillTerminate(aNotification: NSNotification) {
+        NSColorPanel.shared.close()
+    }
+    
+    @IBAction func closeWindow(_ sender: AnyObject?) {
+        NSColorPanel.shared.close()
+    }
+    
+    func windowShouldClose(_ sender: NSWindow) -> Bool {
+        if sender == NSColorPanel.shared {
+            self.lastMode = NSColorPanel.shared.mode
+        }
+        return true
+    }
+    
+    @objc @IBAction func openPalleteFolder(sender: AnyObject?){
+        let showFolder = Process()
 
         showFolder.launchPath = "/usr/bin/open"
         showFolder.arguments = [NSHomeDirectory() + "/Library/Colors"]
